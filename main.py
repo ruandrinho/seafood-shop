1a8d689ef73186c557119a0524c45f0740561cd5
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-08-17 14:37:16 +0500

    Return to ptb 13.13 synchronous library

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from textwrap import dedent
 from contextlib import suppress
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
-    Application,
     CallbackQueryHandler,
     CommandHandler,
     ConversationHandler,
     MessageHandler,
     PicklePersistence,
-    filters,
+    Updater,
+    Filters,
 )
 
 logger = logging.getLogger(__name__)
@@ -21,37 +21,37 @@ logger = logging.getLogger(__name__)
 START, HANDLE_MENU, HANDLE_PRODUCT, HANDLE_CART, AWAIT_EMAIL = range(5)
 
 
-async def start(update, context):
+def start(update, context):
     # logger.info(context.bot_data['moltin_client_id'])
     keyboard = []
     for product in moltin.get_all_products():
         keyboard.append([InlineKeyboardButton(product['name'], callback_data=product['id'])])
     keyboard.append([InlineKeyboardButton('Корзина', callback_data='cart')])
-    await update.message.reply_text(
+    update.message.reply_text(
         'Добро пожаловать в магазин. Выберите товар:',
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
     return HANDLE_MENU
 
 
-async def show_menu(update, context):
+def show_menu(update, context):
     query = update.callback_query
-    await query.answer()
+    query.answer()
     keyboard = []
     for product in moltin.get_all_products():
         keyboard.append([InlineKeyboardButton(product['name'], callback_data=product['id'])])
     keyboard.append([InlineKeyboardButton('Корзина', callback_data='cart')])
-    await query.message.reply_text(
+    query.message.reply_text(
         'Выберите товар:',
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
-    await query.message.delete()
+    query.message.delete()
     return HANDLE_MENU
 
 
-async def show_menu_after_product(update, context):
+def show_menu_after_product(update, context):
     query = update.callback_query
-    await query.answer()
+    query.answer()
     cart_product_id, cart_product_quantity = query.data.split('=')
     with suppress(requests.exceptions.HTTPError):
         moltin.add_product_to_cart(cart_product_id, int(cart_product_quantity), query.from_user.id)
@@ -59,17 +59,17 @@ async def show_menu_after_product(update, context):
     for product in moltin.get_all_products():
         keyboard.append([InlineKeyboardButton(product['name'], callback_data=product['id'])])
     keyboard.append([InlineKeyboardButton('Корзина', callback_data='cart')])
-    await query.message.reply_text(
+    query.message.reply_text(
         'Выберите товар:',
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
-    await query.message.delete()
+    query.message.delete()
     return HANDLE_MENU
 
 
-async def show_product(update, context):
+def show_product(update, context):
     query = update.callback_query
-    await query.answer()
+    query.answer()
     product = moltin.get_product(query.data)
     keyboard = []
     if product['stock'] > 0:
@@ -90,18 +90,18 @@ async def show_product(update, context):
 
         {product["description"]}
         '''
-    await query.message.reply_photo(
+    query.message.reply_photo(
         photo=product['image_url'],
         caption=dedent(message),
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
-    await query.message.delete()
+    query.message.delete()
     return HANDLE_PRODUCT
 
 
-async def show_cart(update, context):
+def show_cart(update, context):
     query = update.callback_query
-    await query.answer()
+    query.answer()
     if query.data != 'cart':
         moltin.remove_product_from_cart(query.data, query.from_user.id)
     cart_products, cart_cost, cart_summary = moltin.get_cart_data(query.from_user.id)
@@ -116,27 +116,27 @@ async def show_cart(update, context):
         message = f'{cart_summary}Total: {cart_cost}'
     else:
         message = 'Товаров пока нет.'
-    await query.message.reply_text(
+    query.message.reply_text(
         message,
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
-    await query.message.delete()
+    query.message.delete()
     return HANDLE_CART
 
 
-async def ask_for_email(update, context):
+def ask_for_email(update, context):
     query = update.callback_query
-    await query.answer()
-    await query.message.reply_text(
+    query.answer()
+    query.message.reply_text(
         'Пожалуйста, введите email для оформления покупки'
     )
-    await query.message.delete()
+    query.message.delete()
     return AWAIT_EMAIL
 
 
-async def finish(update, context):
+def finish(update, context):
     email = update.message.text
-    await update.message.reply_text(
+    update.message.reply_text(
         f'Вы прислали почту {email}. Скоро с вами свяжутся наши менеджеры!'
     )
     moltin.save_customer(email, update.message.from_user)
@@ -151,9 +151,11 @@ def main():
         level=logging.INFO
     )
 
-    token = os.getenv('TELEGRAM_TOKEN')
-    persistence = PicklePersistence(filepath='conversationbot')
-    application = Application.builder().token(token).persistence(persistence).build()
+    updater = Updater(
+        os.getenv('TELEGRAM_TOKEN'),
+        persistence=PicklePersistence(filename='conversationbot')
+    )
+    dispatcher = updater.dispatcher
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
@@ -173,7 +175,7 @@ def main():
                 CallbackQueryHandler(show_menu_after_product)
             ],
             AWAIT_EMAIL: [
-                MessageHandler(filters.TEXT & ~filters.COMMAND, finish)
+                MessageHandler(Filters.text & ~Filters.command, finish)
             ]
         },
         fallbacks=[],
@@ -182,8 +184,10 @@ def main():
     )
 
     # application.bot_data['moltin_client_id'] = os.getenv('MOLTIN_CLIENT_ID')
-    application.add_handler(conv_handler)
-    application.run_polling()
+    dispatcher.add_handler(conv_handler)
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
